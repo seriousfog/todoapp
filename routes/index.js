89017907de6fd6195885aa7a0c978b9182bda602
t9uo <t9uo@fresnou.org>
2023-11-06 09:25:31 -0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const toDoController = require("../controllers/todo_controller")
-
+const toDoController = require("../controllers/todo_controller.js")
 
 /* GET home page. */
 router.get('/', toDoController.homeRoute);
@@ -19,5 +18,4 @@ router.get('/delete/:id', toDoController.deleteItem);
 router.get('/edit/:id', toDoController.renderEditForm);
 
 router.post('/edit/:id', toDoController.updateItem);
-
 module.exports = router;
